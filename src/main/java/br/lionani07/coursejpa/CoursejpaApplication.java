c0263bb41d4926acb411a5367b99b9c90d3c0304
Liorge Corria Sanchez <lionani07@gmail.com>
2020-03-08 13:54:08 -0300

    Project created

---
+++ src/main/java/br/lionani07/coursejpa/CoursejpaApplication.java
@@ -0,0 +1,13 @@
+package br.lionani07.coursejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursejpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/lionani07/coursejpa/CoursejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lionani07.coursejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursejpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
